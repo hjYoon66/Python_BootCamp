34200c11bce2693f27f01262582248f2c41ea21d
Yoon_98 <poiu601@naver.com>
2023-06-24 01:19:44 +0900

    Day2

---
+++ Day2/2-3.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+remaining_years=90-int(age)
+days=remaining_years*365
+weeks=remaining_years*52
+months=remaining_years*12
+print(f"You have {days} days, {weeks} weeks and {months} months left")
